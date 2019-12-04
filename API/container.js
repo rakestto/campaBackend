41a14c6d601af1024b2ead4ba2d23054e8af8837
Jose Fernando <josefergarpa@gmail.com>
2019-12-04 08:46:51 +0100

    Arreglado singleton vehiculo cliente

--- API/container.js
+++ API/container.js
@@ -1,45 +1,45 @@
-const { asClass, asFunction, asValue, createContainer } = require('awilix');
+const { asClass, asFunction, asValue, createContainer } = require("awilix");
 const Container = createContainer();
 
 //Servidor
-const Server = require('./Server');
-const config = require('../Config/config.json');
-const db = require('../Data/models/index');
+const Server = require("./Server");
+const config = require("../Config/config.json");
+const db = require("../Data/models/index");
 
 //Rutas
-const router = require('./Routes/index');
-const VehiculoRoutes = require('./Routes/Vehiculo.routes');
-const ClienteRoutes = require('./Routes/Cliente.routes');
-const FacturaRoutes = require('./Routes/Factura.routes');
-const VehiculoClienteRoutes = require('./Routes/VehiculoClientes.routes');
-const InteresRoutes = require('./Routes/Interes.routes');
+const router = require("./Routes/index");
+const VehiculoRoutes = require("./Routes/Vehiculo.routes");
+const ClienteRoutes = require("./Routes/Cliente.routes");
+const FacturaRoutes = require("./Routes/Factura.routes");
+const VehiculoClienteRoutes = require("./Routes/VehiculoClientes.routes");
+const InteresRoutes = require("./Routes/Interes.routes");
 
 //Controladores
-const VehiculoController = require('./Controllers/Vehiculo.controller');
-const ClienteController = require('./Controllers/Cliente.controller');
-const FacturaController = require('./Controllers/Factura.controller');
-const VehiculoClienteController = require('./Controllers/VehiculoCliente.controller');
-const InteresController = require('./Controllers/Interes.controller');
+const VehiculoController = require("./Controllers/Vehiculo.controller");
+const ClienteController = require("./Controllers/Cliente.controller");
+const FacturaController = require("./Controllers/Factura.controller");
+const VehiculoClienteController = require("./Controllers/VehiculoCliente.controller");
+const InteresController = require("./Controllers/Interes.controller");
 //Servicios SERVICES LAYER
-const VehiculoService = require('../Services/Vehiculo.service');
-const ClienteService = require('../Services/Cliente.service');
-const FacturaService = require('../Services/Factura.service');
-const VehiculoClienteService = require('../Services/VehiculoCliente.service');
-const InteresService = require('../Services/Interes.service');
+const VehiculoService = require("../Services/Vehiculo.service");
+const ClienteService = require("../Services/Cliente.service");
+const FacturaService = require("../Services/Factura.service");
+const VehiculoClienteService = require("../Services/VehiculoCliente.service");
+const InteresService = require("../Services/Interes.service");
 
 //Logica de negocio DOMAIN LAYER
-const VehiculoBussines = require('../Domain/Vehiculo.bussines');
-const ClienteBussines = require('../Domain/Cliente.bussines');
-const FacturaBussines = require('../Domain/Factura.bussines');
-const VehiculoClienteBussines = require('../Domain/VehiculoCliente.bussines');
-const InteresBussines = require('../Domain/Interes.bussines');
+const VehiculoBussines = require("../Domain/Vehiculo.bussines");
+const ClienteBussines = require("../Domain/Cliente.bussines");
+const FacturaBussines = require("../Domain/Factura.bussines");
+const VehiculoClienteBussines = require("../Domain/VehiculoCliente.bussines");
+const InteresBussines = require("../Domain/Interes.bussines");
 
 //Repositorios DATA ACCESS LAYER
-const VehiculoRepository = require('../Data/repositories/Vehiculo.repository');
-const ClienteRepository = require('../Data/repositories/Cliente.repository');
-const FacturaRepository = require('../Data/repositories/Factura.repository');
-const VehiculoClienteRepository = require('../Data/repositories/VehiculoCliente.repository');
-const InteresRepository = require('../Data/repositories/Interes.repository');
+const VehiculoRepository = require("../Data/repositories/Vehiculo.repository");
+const ClienteRepository = require("../Data/repositories/Cliente.repository");
+const FacturaRepository = require("../Data/repositories/Factura.repository");
+const VehiculoClienteRepository = require("../Data/repositories/VehiculoCliente.repository");
+const InteresRepository = require("../Data/repositories/Interes.repository");
 
 Container.register({
   app: asClass(Server).singleton(),
@@ -60,7 +60,7 @@ Container.register({
     VehiculoController: asClass(VehiculoController).singleton(),
     ClienteController: asClass(ClienteController).singleton(),
     FacturaController: asClass(FacturaController).singleton(),
-    VehiculoClienteController: asClass(VehiculoClienteController),
+    VehiculoClienteController: asClass(VehiculoClienteController).singleton(),
     InteresController: asClass(InteresController).singleton()
   })
   .register({
